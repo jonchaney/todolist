5d8796a7a01809f8f089ef3220a96a7daed460f9
Jonathan Chaney <jonchaney@gmail.com>
2017-10-08 15:02:19 -0700

    add items to list

--- lib/js/entry.jsx
+++ lib/js/entry.jsx
@@ -1,4 +1,9 @@
+import TodoList from './TodoList.js';
 document.addEventListener('DOMContentLoaded', () => {
+
+  let list = new TodoList();
   
+  window.list = list;
+
   console.log('content loaded');
 });
