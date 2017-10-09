418a72f940f29905ee87f835203eecc6ac885ae2
Jonathan Chaney <jonchaney@gmail.com>
2017-10-09 14:45:39 -0700

    additems and remove button

--- lib/js/entry.jsx
+++ lib/js/entry.jsx
@@ -1,9 +1,7 @@
 import TodoList from './TodoList.js';
 document.addEventListener('DOMContentLoaded', () => {
 
-  let list = new TodoList();
+  window.list = new TodoList();
   
-  window.list = list;
-
   console.log('content loaded');
 });
